464b896f51f9e33d34ae5f97c64512fd86286b64
Christian Mendoza <christianmendozater@gmail.com>
2022-12-10 21:55:51 -0400

    preguntas corregidas

--- data/data2.js
+++ data/data2.js
@@ -2,66 +2,66 @@ export const data2 = [
     {
       question: "PreguntaRandom",
       choices: ["Intuitivo", "Sensorial"],
-      answer: ["Intuitivo", "Intuitivo"]
+      answer: ["Intuitivo", "Sensorial"]
     },
     {
       question: "PreguntaRandom",
       choices: ["Intuitivo", "Sensorial"],
-      answer: ["Intuitivo", "Intuitivo"]
-    },
-    
-    {
-      question: "PreguntaRandom",
-      choices: ["Intuitivo", "Sensorial"],
-      answer: ["Intuitivo", "Intuitivo"]
+      answer: ["Intuitivo", "Sensorial"]
     },
     
     {
       question: "PreguntaRandom",
       choices: ["Intuitivo", "Sensorial"],
-      answer: ["Intuitivo", "Intuitivo"]
+      answer: ["Intuitivo", "Sensorial"]
     },
     
     {
       question: "PreguntaRandom",
       choices: ["Intuitivo", "Sensorial"],
-      answer: ["Intuitivo", "Intuitivo"]
+      answer: ["Intuitivo", "Sensorial"]
     },
     
     {
       question: "PreguntaRandom",
       choices: ["Intuitivo", "Sensorial"],
-      answer: ["Intuitivo", "Intuitivo"]
+      answer: ["Intuitivo", "Sensorial"]
     },
     
     {
       question: "PreguntaRandom",
       choices: ["Intuitivo", "Sensorial"],
-      answer: ["Intuitivo", "Intuitivo"]
+      answer: ["Intuitivo", "Sensorial"]
     },
     
     {
       question: "PreguntaRandom",
       choices: ["Intuitivo", "Sensorial"],
-      answer: ["Intuitivo", "Intuitivo"]
+      answer: ["Intuitivo", "Sensorial"]
     },
     
     {
       question: "PreguntaRandom",
       choices: ["Intuitivo", "Sensorial"],
-      answer: ["Intuitivo", "Intuitivo"]
+      answer: ["Intuitivo", "Sensorial"]
     },
     
     {
       question: "PreguntaRandom",
       choices: ["Intuitivo", "Sensorial"],
-      answer: ["Intuitivo", "Intuitivo"]
+      answer: ["Intuitivo", "Sensorial"]
     },
     
     {
       question: "PreguntaRandom",
       choices: ["Intuitivo", "Sensorial"],
-      answer: ["Intuitivo", "Intuitivo"]
+      answer: ["Intuitivo", "Sensorial"]
+    },
+    
+    {
+      question: "PreguntaRandom",
+      choices: ["Intuitivo", "Sensorial"],
+      answer: ["Intuitivo", "Sensorial"]
     }
     
 ];
